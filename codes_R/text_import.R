ccf54194ea572b92cad5230147f99aad2d2181d3
KNEFATI Anas <AKNEFATI@DOMENSAI.ECOLE>
2019-10-22 10:07:55 +0200

    add script of text import

--- codes_R/text_import.R
+++ codes_R/text_import.R
@@ -1,15 +1,32 @@
+project_directory = "P:\\suivi_projets\\VisLang\\VizLang_v2\\" #"P:\\suivi_projets\\VisLang\\VisLang\\" 
+setwd(project_directory)
 
 ### Dossier où l'archive zip de Moodle a été placée
-
-fullpath <- "D:/Home/alafonta/Desktop/Nouveau dossier"
+fullpath <- paste0(project_directory, "texts")#"D:/Home/alafonta/Desktop/Nouveau dossier"
 setwd(fullpath)
 
+# detect the os name
+get_sys_name = function(){
+  return(Sys.info()["sysname"])
+}
+
+os = get_sys_name()
+if (os == "Windows"){
+  project_directory = "P:\\suivi_projets\\VisLang\\VizLang_v2\\" #"P:\\suivi_projets\\VisLang\\VisLang\\" 
+  sep = "\\"
+}else{
+  project_directory = "/home/knefati/Documents/MyWork-Ensai/VizLing/"
+  sep = "/"
+}
 
 ### Extraction de l'archive
 
-zipF <- paste(fullpath, list.files(), sep = "/")
-outDir<-str_split(zipF,pattern = ".zip")[[1]][1]
-unzip(zipF,exdir=outDir)
+zipF <- paste(fullpath, list.files(), sep = sep)
+outDir<- unlist(strsplit(zipF,split  = ".zip"))[1]         
+
+if (!dir.exists(outDir)){
+  unzip(zipF,exdir=outDir)
+}
 
 
 ### Importation des fichiers html
@@ -22,14 +39,10 @@ directorynames <- basename(manydirectories)
 directorynames <- directorynames[2:length(directorynames)]
 
 names <- c()
-
+texte <- c()
 for(i in 1:length(directorynames)){
   names[i] <- str_split(string = directorynames[i], pattern = "_")[[1]][1]
-}
-
-texte <- c()
-
-for(i in 1:length(directorynames)){
+  
   texte[i] <- paste(readLines(paste(fullpath, directorynames[i],"onlinetext.html",sep = "/"), warn=F), collapse="")
 }
 
