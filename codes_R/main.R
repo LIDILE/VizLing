1fdecfc26a5911b3dd3bc9723288bf61d73be468
alafontaine <52162367+alafontaine@users.noreply.github.com>
2020-01-30 11:23:51 +0100

    Update main.R
    Add possibility to read csv files from CELVA.sp

--- codes_R/main.R
+++ codes_R/main.R
@@ -42,6 +42,10 @@ path_feedbacks= "feedbacks"
 parsedFiles = "ParsedFiles"
 
 data_from_csv = FALSE #true if .csv and FALSE if .zip from MOODLE
+CELVA.sp = FALSE      # true if .csv and from CELVA.sp
+var_texte = "texte"   # name of the variable containing the text if .csv and not from CELVA.sp
+var_id = "id"         # name of the variable containing IDs if .csv and not from CELVA.sp
+
 desc_stat_chart = "boxplot" # possible parameters: c("boxplot",  "violin",   "boxplot_point" )
 check_creat_directory(path_feedbacks)
 
