a592992c1878b91d96c16f95a22f1cfb0edc2038
alafontaine <52162367+alafontaine@users.noreply.github.com>
2020-02-19 11:00:35 +0100

    Update text_import.R
    Use of list.files() instead of "onlinetext.html" (indeed, if Moodle is settled in French, the correct name would be "texteenligne.html")

--- codes_R/text_import.R
+++ codes_R/text_import.R
@@ -26,13 +26,24 @@ manydirectories <- list.dirs()
 directorynames <- basename(manydirectories)
 directorynames <- directorynames[2:length(directorynames)]
 
+#names <- c()
+#text <- c()
+#for(i in 1:length(directorynames)){
+#  names[i] <- str_split(string = directorynames[i], pattern = "_")[[1]][1]
+#  names[i] <- gsub(" ", "_", names[i])
+#  
+#  text[i] <- paste(readLines(paste(outDir, directorynames[i],"onlinetext.html",sep = sep), warn=F), collapse="")
+#}
+
 names <- c()
 text <- c()
 for(i in 1:length(directorynames)){
   names[i] <- str_split(string = directorynames[i], pattern = "_")[[1]][1]
   names[i] <- gsub(" ", "_", names[i])
   
-  text[i] <- paste(readLines(paste(outDir, directorynames[i],"onlinetext.html",sep = sep), warn=F), collapse="")
+  setwd(paste(outDir, directorynames[i],sep = sep))
+  text[i] <- paste(readLines(paste(outDir, directorynames[i],list.files(),sep = sep), warn=F), collapse="")
+  setwd(outDir)
 }
 
 base_f <- data.frame(doc_id=names,text=text)
